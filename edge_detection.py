e9b602bfc2333df4edaeef87eecebc0f4d4453cc
Badrinath Reddy Gopugari <badrinathgopugari@gmail.com>
2023-03-22 15:07:21 -0500

    init

--- edge_detection.py
+++ edge_detection.py
@@ -1,11 +1,8 @@
 import cv2
 
 
-def edge_detection(img):
-    # threshold
-    t = 50
+def edge_detection(img, t=50):
     original_img = img.copy()
-    # canny edge detection
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.GaussianBlur(img, (5, 5), 0)
     img = cv2.Canny(img, t, t*2)
