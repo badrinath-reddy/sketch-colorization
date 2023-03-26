3f476de066caff46684eca10773a5dc163224e97
Badrinath Reddy Gopugari <badrinathgopugari@gmail.com>
2023-03-25 19:43:49 -0500

    init

--- train.py
+++ train.py
@@ -3,8 +3,8 @@ from torch import nn
 from models.simplegan import Generator, Discriminator
 from utils import *
 from data_loader import *
-import os
 from torch.utils.tensorboard import SummaryWriter
+import os
 
 import argparse
 
@@ -22,19 +22,19 @@ parser.add_argument("--b2", type=float, default=0.999,
                     help="adam: decay of first order momentum of gradient")
 parser.add_argument("--decay_epoch", type=int, default=100,
                     help="epoch from which to start lr decay")
-parser.add_argument("--n_cpu", type=int, default=8,
-                    help="number of cpu threads to use during batch generation")
-parser.add_argument("--checkpoint_interval", type=int,
-                    default=10, help="interval between model checkpoints")
 opt = parser.parse_args()
 print(opt)
+
+
+cwd = os.getcwd()
+os.makedirs(f"{cwd}/saved_models", exist_ok=True)
 
 device = get_device()
 
 writer = SummaryWriter()
 
-ganloss = nn.MSELoss()
-descloss = nn.L1Loss()
+ganloss = nn.MSELoss().to(device)
+descloss = nn.L1Loss().to(device)
 
 
 lambda_pixel = 100
