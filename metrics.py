44bb8eedd3bb6e68ca6df67755dca6201b8561f2
Badrinath Reddy Gopugari <badrinathgopugari@gmail.com>
2023-04-10 10:53:10 -0500

    init

--- metrics.py
+++ metrics.py
@@ -8,7 +8,6 @@ def ssim(img1, img2):
     # denormalize
     img1 = denormalize(img1) * 255
     img2 = denormalize(img2) * 255
-    print(img1.min(), img1.max(), img2.min(), img2.max())
     img1 = img1.permute(1, 2, 0).numpy().astype(np.uint8)
     img2 = img2.permute(1, 2, 0).numpy().astype(np.uint8)
     return structural_similarity(img1, img2, multichannel=True, channel_axis=2)
